d3f76dabf6de26d2fa0bcfe4c2a8bca255a778be
michaelavs <michaela.sizemore@gmail.com>
2022-11-16 13:48:54 -0700

    updates

--- test/ci_tests.py
+++ test/ci_tests.py
@@ -2,10 +2,12 @@ from unittest import TestCase
 import pytest
 import numpy as np
 
-class TestCase(TestCase):
+class test_placeholder(TestCase):
     def test_placeholder(self):
         print("hello world")
         a = [0,1,2]
         b = [0,1,2]
 
         assert a==b
+
+print(test_placeholder())
